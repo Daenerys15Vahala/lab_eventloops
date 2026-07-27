3c22d56fc47c25636222a986c5acdf5cb8c38847
Valentina BM <valentina15bermo@gmail.com>
2026-07-27 15:55:22 -0400

    Add files via upload

--- loopslab.js
+++ loopslab.js
@@ -1,17 +1,42 @@
-// ALAB EVENT LOOP
+// // ALAB EVENT LOOP
 
-// part 1 
+// // part 1 
 
 let callCount = 0;
 
 function causeStackOverflow() {
-  callCount++;
-  causeStackOverflow();
+    callCount++;
+    causeStackOverflow();
 }
 
 try {
-  causeStackOverflow();
+    causeStackOverflow();
 } catch (error) {
-  console.error("Error:", error.message);
-  console.log("Maximum number of calls:", callCount);
+    console.error("Error:", error.message);
+    console.log("Maximum number of calls:", callCount);
+}
+
+// part 2
+
+function trampoline(functionToRun) {
+    let result = functionToRun;
+
+    while (typeof result === "function") {
+        result = result();
+    }
+    return result;
+}
+
+function flattenArray(items, flattened = []) {
+    if (items.length === 0) {
+        return flattened;
+    }
+    const currentItem = items.shift();
+
+    if (Array.isArray(currentItem)) {
+        items.unshift(...currentItem);
+    } else {
+        flattened.push(currentItem);
+    }
+    return () => flattenArray(items, flattened);
 }
