5f26997807833af86e08a2d3f5169b7945f4d423
Valentina BM <valentina15bermo@gmail.com>
2026-07-27 15:16:42 -0400

    Add files via upload

--- loopslab.js
+++ loopslab.js
@@ -1,2 +1,17 @@
 // ALAB EVENT LOOP
 
+// part 1 
+
+let callCount = 0;
+
+function causeStackOverflow() {
+  callCount++;
+  causeStackOverflow();
+}
+
+try {
+  causeStackOverflow();
+} catch (error) {
+  console.error("Error:", error.message);
+  console.log("Maximum number of calls:", callCount);
+}
